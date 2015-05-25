943d03f4ca868b957ea7ffe21c19377d3f38638f
jozsefklemasz <tomleidenofficial@gmail.com>
2015-05-25 21:23:40 +0200

    cleanup

--- catalog/controller/notexists.php
+++
@@ -1,8 +0,0 @@
-<?php
-class NotExistsController Extends Controller{
-
-	public function Index(){
-		$this->response->SetOutput('404.tpl');
-	}
-
-}
--- catalog/view/example.tpl
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to Ghost!</h1>
-<p align="center">Please check the README for more.</p>
