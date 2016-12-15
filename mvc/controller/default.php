b91fd8c703b78b99a90c80a6c559b56711a767ba
Jozsef Klemasz <jozsefklemasz@gmail.com>
2016-12-15 22:39:12 +0100

    cleanup

--- mvc/controller/default.php
+++ mvc/controller/default.php
@@ -2,15 +2,6 @@
 
 class DefaultController extends Controller{
 	public function Index(){
-		if($this->request->post){
-			$this->user->Login($this->request->post);
-		}
-
-		if($this->user->LoggedIn())
-		{
-			$this->data['loggedin'] = true;
-		}
-
 		$this->data['header'] = $this->load->controller('blocks/header');
 		$this->data['footer'] = $this->load->controller('blocks/footer');
 		$this->response->SetOutput('default.tpl');
